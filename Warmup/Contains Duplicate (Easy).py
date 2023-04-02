1f6c39e82b99be3bb76ba99ba675b66fc402dbb9
Adam Alam <adamalam7@outlook.com>
2023-04-01 22:30:49 -0500

    Revert "Initial commit"
    This reverts commit 0ddcd64f7a4cb114b9d2916f8b420ba65fd58605.

--- Warmup/Contains Duplicate (Easy).py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def containsDuplicate(self, nums):
-        check_set = set()
-        for num in nums:
-            if num in check_set:
-                return True
-            check_set.add(num)
-        return False
